ef80bd19831cdb0cbc81988c073452be188d92d1
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2023-03-23 23:25:21 -0300

    feat: integrates kong to keycloak to provide authentication

---
+++ infra/kong-k8s/misc/token/get-token.sh
@@ -0,0 +1,10 @@
+kubectl exec -it testcurl -- sh
+
+curl --location --request POST 'http://keycloak.iam/realms/bets/protocol/openid-connect/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'client_id=kong' \
+--data-urlencode 'grant_type=password' \
+--data-urlencode 'username=maria' \
+--data-urlencode 'password=maria' \
+--data-urlencode 'client_secret=QaMsNrZeeCea4QEiCxxQZirtveuZKA5y' \
+--data-urlencode 'scope=openid'
